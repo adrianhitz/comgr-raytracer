829839eae625e481d97d7d331c084858f8dc69fb
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-09-24 16:58:27 +0200

    Changed property Sphere.R from double to float to be more consistent with the rest of the code

--- Raytracing/Sphere.cs
+++ Raytracing/Sphere.cs
@@ -3,10 +3,10 @@
 namespace Raytracing {
     class Sphere : ISceneObject {
         public Vector3 Centre { get; }
-        public double R { get; }
+        public float R { get; }
         public Colour Colour { get; }
 
-        public Sphere(Vector3 centre, double r, Colour colour) {
+        public Sphere(Vector3 centre, float r, Colour colour) {
             this.Centre = centre;
             this.R = r;
             this.Colour = colour;
