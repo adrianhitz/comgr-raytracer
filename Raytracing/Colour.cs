fa0ca7e995607f9ad984ad11f6db6edd54e4c5f0
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-09-24 16:50:13 +0200

    Added some predefined colours to class 'Colour'

--- Raytracing/Colour.cs
+++ Raytracing/Colour.cs
@@ -6,6 +6,16 @@ namespace Raytracing {
         public float G { get; }
         public float B { get; }
 
+        public readonly Colour Black = new Colour(0, 0, 0);
+        public readonly Colour Red = new Colour(1, 0, 0);
+        public readonly Colour Green = new Colour(0, 1, 0);
+        public readonly Colour Blue = new Colour(0, 0, 1);
+        public readonly Colour Yellow = new Colour(1, 1, 0);
+        public readonly Colour Magenta = new Colour(1, 0, 1);
+        public readonly Colour Cyan = new Colour(0, 1, 1);
+        public readonly Colour White = new Colour(1, 1, 1);
+        public readonly Colour LightCyan = new Colour(0.8f, 1, 1);
+
         public Colour(float r, float g, float b) {
             this.R = r;
             this.G = g;
@@ -15,5 +25,8 @@ namespace Raytracing {
         public Vector3 ToVector() {
             return new Vector3(R, G, B);
         }
+
+
+
     }
 }
