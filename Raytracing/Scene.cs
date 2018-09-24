dae4cbc1fd496f678e0ca8e43b49ef6ef1adaa62
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-09-24 17:00:32 +0200

    Added predefined Cornell Box scene to class 'Scene'

--- Raytracing/Scene.cs
+++ Raytracing/Scene.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Numerics;
 
 namespace Raytracing {
     public class Scene {
@@ -10,6 +11,23 @@ namespace Raytracing {
 
         public void AddObjects(IEnumerable<ISceneObject> sceneObjects) {
             SceneObjects.AddRange(sceneObjects);
+        }
+
+        private static Scene CornellBox() {
+            Scene cornellBox = new Scene();
+
+            cornellBox.AddObjects(new Sphere[] {
+                new Sphere(new Vector3(-1001, 0, 0), 1000, Colour.Red),
+                new Sphere(new Vector3(1001, 0, 0), 1000, Colour.Blue),
+                new Sphere(new Vector3(0, 0, 1001), 1000, Colour.White),
+                new Sphere(new Vector3(0, -1001, 0), 1000, Colour.White),
+                new Sphere(new Vector3(0, 1001, 0), 1000, Colour.White),
+                new Sphere(new Vector3(-0.6f, 0.7f, -0.6f), 0.3f, Colour.Yellow),
+                new Sphere(new Vector3(0.3f, 0.4f, 0.3f), 0.6f, Colour.LightCyan)
+            });
+
+            return cornellBox;
+
         }
     }
 }
