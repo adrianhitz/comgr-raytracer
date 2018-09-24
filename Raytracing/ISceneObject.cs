610ede05c9b3aa137250fc51de9f20dd392c30e6
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-09-24 19:42:40 +0200

    Implemented class HitPoint

--- Raytracing/ISceneObject.cs
+++ Raytracing/ISceneObject.cs
@@ -1,4 +1,32 @@
-﻿namespace Raytracing {
+﻿using System;
+using System.Numerics;
+
+namespace Raytracing {
     public interface ISceneObject {
+        HitPoint CalculateHitPoint(Ray ray);
+    }
+
+    public class HitPoint {
+        public bool Exists { get; }
+        public float Lambda {
+            get {
+                if(Exists) return Lambda;
+                throw new InvalidOperationException("No hitpoint found. Can't access Lambda.");
+            }
+            private set {
+                Lambda = value;
+            }
+        }
+        public static HitPoint None = new HitPoint();
+
+        private HitPoint() {
+            this.Exists = false;
+            this.Lambda = 0;
+        }
+
+        public HitPoint(float lambda) {
+            this.Exists = true;
+            this.Lambda = lambda;
+        }
     }
 }
