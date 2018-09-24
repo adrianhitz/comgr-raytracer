fac219110f92673c9e857246b9aaff779e3247f0
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-09-24 14:08:01 +0200

    Added classes 'Camera' and 'Scene'

---
+++ Raytracing/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace Raytracing {
+    class Camera {
+        public Vector3 Position { get; }
+        public Vector3 Up { get; }
+        public Vector3 LookAt { get; }
+        public float FOV { get; }
+
+        public Camera(Vector3 position, Vector3 lookAt, float fov) : this(position, lookAt, fov, new Vector3(0, 1, 0)) { }
+
+        public Camera(Vector3 position, Vector3 lookAt, float fov, Vector3 up) {
+            this.Position = position;
+            this.LookAt = lookAt;
+            this.Up = up;
+            this.FOV = fov;
+        }
+    }
+}
---
+++ Raytracing/Scene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Raytracing {
+    class Scene {
+        private List<Sphere> spheres;
+        private Camera Camera { get; }
+
+        public Scene(Camera camera) {
+            this.spheres = new List<Sphere>();
+        }
+
+        public void AddSphere(Sphere sphere) {
+            spheres.Add(sphere);
+        }
+
+        public void AddSpheres(IEnumerable<Sphere> s) {
+            spheres.AddRange(s);
+        }
+    }
+}
