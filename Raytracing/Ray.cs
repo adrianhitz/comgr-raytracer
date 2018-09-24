036d0585dbd93418477b9b35f203e66465fbf79a
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-09-24 14:07:35 +0200

    Added class 'Ray'

---
+++ Raytracing/Ray.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+namespace Raytracing {
+    class Ray {
+        public Vector3 Origin { get; }
+        public Vector3 Direction { get; }
+
+        public Ray(Vector3 origin, Vector3 direction) {
+            this.Origin = origin;
+            this.Direction = Vector3.Normalize(direction);
+        }
+    }
+}
