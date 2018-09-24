71115225703065859dc1cd319706f4988bd170ba
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-09-24 14:01:04 +0200

    Added class 'Colour'

---
+++ Raytracing/Colour.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace Raytracing {
+    class Colour {
+        public float R { get; }
+        public float G { get; }
+        public float B { get; }
+
+        public Colour(float r, float g, float b) {
+            this.R = r;
+            this.G = g;
+            this.B = b;
+        }
+
+        public Vector3 ToVector() {
+            return new Vector3(R, G, B);
+        }
+    }
+}
