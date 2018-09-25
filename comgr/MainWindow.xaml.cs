012e8a1f534972b62c4e67e0d6b28c703131d011
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-09-25 14:16:21 +0200

    Added Raytracer call to main window

--- comgr/MainWindow.xaml.cs
+++ comgr/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Raytracing;
 
 namespace comgr {
     /// <summary>
@@ -20,6 +23,17 @@ namespace comgr {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+
+            int width = 400;
+            int height = 400;
+            Image image = new Image();
+            WriteableBitmap writeableBitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgr32, null);
+            image.Source = writeableBitmap; ;
+            grid.Children.Add(image);
+
+            Raytracer raytracer = new Raytracer(Camera.CornellBoxCamera(), Scene.CornellBox(), width, height);
+            var pixels = raytracer.CalculatePixelsByteArray();
+            writeableBitmap.WritePixels(new Int32Rect(0, 0, width, height), pixels, width * 4, 0);
         }
     }
 }
