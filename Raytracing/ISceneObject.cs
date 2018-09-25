2dbdd8699d770d87616787c593db1921dde20f9f
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-09-25 12:13:59 +0200

    Added additional properties to 'ISceneObject'

--- Raytracing/ISceneObject.cs
+++ Raytracing/ISceneObject.cs
@@ -1,14 +1,20 @@
-﻿namespace Raytracing {
+﻿using System.Numerics;
+
+namespace Raytracing {
     public interface ISceneObject {
         HitPoint? CalculateHitPoint(Ray ray);
     }
 
     public struct HitPoint {
         public float Lambda { get; }
+        public Vector3 Location { get; }
+        public Vector3 Normal { get; }
         public ISceneObject HitObject { get; }
-        
-        public HitPoint(float lambda, ISceneObject hitObject) {
+
+        public HitPoint(float lambda, Vector3 location, Vector3 normal, ISceneObject hitObject) {
             this.Lambda = lambda;
+            this.Location = location;
+            this.Normal = normal;
             this.HitObject = hitObject;
         }
     }
