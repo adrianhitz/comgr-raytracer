6648fc27446a6d09109064010388cffa45be5ed2
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-09-25 10:38:21 +0200

    Implemented class 'LightSource'

---
+++ Raytracing/LightSource.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+namespace Raytracing {
+    class LightSource {
+        public Vector3 Position { get; }
+        public Colour Colour { get; }
+
+        public LightSource(Vector3 position, Colour colour) {
+            this.Position = position;
+            this.Colour = colour;
+        }
+    }
+}
