ef280ccf0feaea983ed6fd59ef4d5adc3eec775b
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-09-29 13:49:43 +0200

    Refactored Sphere.CalculateHitPoint

--- Raytracing/Sphere.cs
+++ Raytracing/Sphere.cs
@@ -28,20 +28,17 @@ namespace Raytracing {
             float b = 2 * Vector3.Dot(CE, ray.Direction);
             float c = (float)(Math.Pow(CE.Length(), 2) - Math.Pow(R, 2));
             double discriminant = b * b - 4 * a * c;
-            if(discriminant < 0) {
-                return null;
-            } else {
+            if(discriminant >= 0) {
                 float solution1 = (float)((-b + Math.Sqrt(discriminant)) / (2 * a));
                 float solution2 = (float)((-b - Math.Sqrt(discriminant)) / (2 * a));
-                if(solution1 < 0 && solution2 < 0) {
-                    return null;
-                } else {
+                if(solution1 > 0 || solution2 > 0) {
                     float lambda = solution1 >= 0 && solution2 >= 0 ? Math.Min(solution1, solution2) : Math.Max(solution1, solution2);
                     Vector3 hitPointPosition = ray.Origin + lambda * ray.Direction;
                     Vector3 normal = Vector3.Normalize(hitPointPosition - Centre);
                     return new HitPoint(lambda, hitPointPosition, normal, this);
                 }
             }
+            return null;
         }
     }
 }
