8028e7f30f6ca56c34c87f2c578aa92fad53878f
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-10-05 19:30:51 +0200

    Implemented Vector3Extensions.FromSRGB

--- Raytracing/Vector3Extensions.cs
+++ Raytracing/Vector3Extensions.cs
@@ -3,9 +3,14 @@ using System.Numerics;
 
 namespace Raytracing {
     public static class Vector3Extensions {
-        public static Vector3 ToSRGB(this Vector3 vector3, float lambda) {
-            float f(float component) => (float)Math.Pow(Math.Max(Math.Min(component, 1), 0), 1 / lambda);
+        public static Vector3 ToSRGB(this Vector3 vector3, float gamma) {
+            float f(float component) => (float)Math.Pow(Math.Max(Math.Min(component, 1), 0), 1 / gamma);
             return 255 * new Vector3(f(vector3.X), f(vector3.Y), f(vector3.Z));
+        }
+
+        public static Vector3 FromSRGB(this Vector3 vector3, float gamma) {
+            float f(float component) => (float)Math.Pow(Math.Max(Math.Min(component, 255), 0) / 255.0, gamma);
+            return new Vector3(f(vector3.X), f(vector3.Y), f(vector3.Z));
         }
     }
 }
