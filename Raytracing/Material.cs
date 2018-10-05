9f1766735154a5ade8b3d07bec81bf6361b55827
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-10-05 19:32:01 +0200

    Moved material components to their own class

---
+++ Raytracing/Material.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace Raytracing {
+    public class Material {
+        public Vector3 Diffuse { get; }
+        public Vector3 Specular { get; }
+        public Vector3 Reflective { get; }
+        public Vector3 Emissive { get; }
+
+        public Material(Vector3 diffuse, Vector3 specular, Vector3 reflective, Vector3 emissive) {
+            this.Diffuse = diffuse;
+            this.Specular = specular;
+            this.Reflective = reflective;
+            this.Emissive = emissive;
+        }
+
+        public Material(Vector3 diffuse, Vector3 specular, Vector3 reflective) : this(diffuse, specular, reflective, Colour.Black) { }
+    }
+}
