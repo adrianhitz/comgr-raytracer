b5683d212dac0f8791d1f4b818c3763a8dfedfa8
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-10-06 10:57:08 +0200

    Renamed the Centre property in Sphere to Position. Added a Position property to ISceneObject.

--- Raytracing/ISceneObject.cs
+++ Raytracing/ISceneObject.cs
@@ -1,5 +1,8 @@
-﻿namespace Raytracing {
+﻿using System.Numerics;
+
+namespace Raytracing {
     public interface ISceneObject {
+        Vector3 Position { get; }
         Material Material { get; }
         Texture Texture { get; }
         HitPoint CalculateHitPoint(Ray ray);
--- Raytracing/Sphere.cs
+++ Raytracing/Sphere.cs
@@ -3,20 +3,20 @@ using System.Numerics;
 
 namespace Raytracing {
     public class Sphere : ISceneObject {
-        public Vector3 Centre { get; }
+        public Vector3 Position { get; }
         public float R { get; }
         public Material Material { get; }
         public Texture Texture { get; }
 
         public Sphere(Vector3 centre, float r, Material material, Texture texture = null) {
-            this.Centre = centre;
+            this.Position = centre;
             this.R = r;
             this.Material = material;
             this.Texture = texture;
         }
 
         public HitPoint CalculateHitPoint(Ray ray) {
-            Vector3 CE = ray.Origin - Centre;
+            Vector3 CE = ray.Origin - Position;
             float a = 1;
             float b = 2 * Vector3.Dot(CE, ray.Direction);
             float c = (float)(Math.Pow(CE.Length(), 2) - Math.Pow(R, 2));
@@ -27,7 +27,7 @@ namespace Raytracing {
                 if(solution1 > 0 || solution2 > 0) {
                     float lambda = solution1 >= 0 && solution2 >= 0 ? Math.Min(solution1, solution2) : Math.Max(solution1, solution2);
                     Vector3 hitPointPosition = ray.Origin + lambda * ray.Direction;
-                    Vector3 normal = Vector3.Normalize(hitPointPosition - Centre);
+                    Vector3 normal = Vector3.Normalize(hitPointPosition - Position);
                     return new HitPoint(lambda, hitPointPosition, normal, this);
                 }
             }
