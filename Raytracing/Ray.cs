b6b1b7a59dc9f2da7c0543ed52255d6b54e971b8
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-10-06 13:27:24 +0200

    Documented Ray

--- Raytracing/Ray.cs
+++ Raytracing/Ray.cs
@@ -1,10 +1,28 @@
 ﻿using System.Numerics;
 
 namespace Raytracing {
+
+    /// <summary>
+    /// Represents a ray. A ray, or a half-line, is a line with an initial point.
+    /// Has an origin and a direction.
+    /// </summary>
     public class Ray {
+
+        /// <summary>
+        /// The ray's origin.
+        /// </summary>
         public Vector3 Origin { get; }
+
+        /// <summary>
+        /// The ray's direction, a normalised vector.
+        /// </summary>
         public Vector3 Direction { get; }
 
+        /// <summary>
+        /// Creates a new ray with given origin and direction.
+        /// </summary>
+        /// <param name="origin">The ray's origin.</param>
+        /// <param name="direction">The ray's direction. Will be normalised by the constructor.</param>
         public Ray(Vector3 origin, Vector3 direction) {
             this.Origin = origin;
             this.Direction = Vector3.Normalize(direction);
