39c01785cb77db4348122df9d64a77e3e3f43c92
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-10-06 13:08:33 +0200

    Documented Scene

--- Raytracing/Scene.cs
+++ Raytracing/Scene.cs
@@ -3,56 +3,115 @@ using System.Collections.Generic;
 using System.Numerics;
 
 namespace Raytracing {
+
+    /// <summary>
+    /// Represents a scene with objects and light sources, that can be drawn by the <see cref="Raytracer"/>.
+    /// </summary>
     public class Scene {
         private List<ISceneObject> SceneObjects { get; }
         private List<LightSource> LightSources { get; }
         private static readonly float HIT_POINT_ADJUSTMENT = 0.01f;
-        public int PhongK { get; set; } = 40;
         private Vector3 ambientLight = Colour.Black;
+        private float shadowBrightness = 0.1f;
+
+        /// <summary>
+        /// The k value used for the calculation of Phong reflection.
+        /// </summary>
+        public int PhongK { get; set; } = 40;
+
+        /// <summary>
+        /// The scene's ambient light. Default is none. Component values outside the range [0,1] will be clamped.
+        /// </summary>
         public Vector3 AmbientLight {
             get => ambientLight;
             set => ambientLight = new Vector3(Math.Min(Math.Max(value.X, 0), 1), Math.Min(Math.Max(value.Y, 0), 1), Math.Min(Math.Max(value.Z, 0), 1));
         }
-        private float shadowBrightness = 0.1f;
+
+        /// <summary>
+        /// Brightness of shadows, with 0 representing completely black shadows and 1 representing no shadows.
+        /// Values outside the range [0,1] will be clamped.
+        /// </summary>
         public float ShadowBrightness {
             get => shadowBrightness;
             set => shadowBrightness = Math.Min(Math.Max(value, 0), 1);
         }
 
+        /// <summary>
+        /// Creates an empty scene.
+        /// </summary>
         public Scene() {
             this.SceneObjects = new List<ISceneObject>();
             this.LightSources = new List<LightSource>();
         }
 
+        /// <summary>
+        /// Creates a scene with a drawable object and a light source.
+        /// </summary>
+        /// <param name="sceneObject">Drawable object to be added to the scene</param>
+        /// <param name="lightSource">Light source to be added to the scene</param>
         public Scene(ISceneObject sceneObject, LightSource lightSource) : this() {
             AddObject(sceneObject);
         }
 
+        /// <summary>
+        /// Creates a scene with multiple drawable objects and a light source.
+        /// </summary>
+        /// <param name="sceneObjects">Drawable objects to be added to the scene</param>
+        /// <param name="lightSource">Light source to be added to the scene</param>
         public Scene(IEnumerable<ISceneObject> sceneObjects, LightSource lightSource) : this() {
             AddObjects(sceneObjects);
         }
 
+        /// <summary>
+        /// Creates a scene with multiple drawable objects and multiple lightsources.
+        /// </summary>
+        /// <param name="sceneObjects">Drawable objects to be added to the scene</param>
+        /// <param name="lightSources">Light sources to be added to the scene</param>
         public Scene(IEnumerable<ISceneObject> sceneObjects, IEnumerable<LightSource> lightSources) {
             AddObjects(sceneObjects);
             AddLightSources(lightSources);
         }
 
+        /// <summary>
+        /// Adds a drawable object to the scene
+        /// </summary>
+        /// <param name="sceneObject">A drawable object</param>
         public void AddObject(ISceneObject sceneObject) {
             SceneObjects.Add(sceneObject);
         }
 
+        /// <summary>
+        /// Adds multiple drawable objects to the scene
+        /// </summary>
+        /// <param name="sceneObjects">Drawable objects</param>
         public void AddObjects(IEnumerable<ISceneObject> sceneObjects) {
             SceneObjects.AddRange(sceneObjects);
         }
 
+        /// <summary>
+        /// Adds a light source to the scene
+        /// </summary>
+        /// <param name="lightSource">A light source</param>
         public void AddLightSource(LightSource lightSource) {
             LightSources.Add(lightSource);
         }
 
+        /// <summary>
+        /// Adds multiple light sources to the scene
+        /// </summary>
+        /// <param name="lightSources">Multiple light sources.</param>
         public void AddLightSources(IEnumerable<LightSource> lightSources) {
             LightSources.AddRange(lightSources);
         }
 
+        /// <summary>
+        /// Finds the closest intersection of a given ray with a drawable object in the scene.
+        /// </summary>
+        /// <param name="ray">The ray, e.g. an eye ray</param>
+        /// <returns>
+        /// A <see cref="HitPoint"/> object containing information about the intersection, including the position in
+        /// three-dimensional space and the surface normal.
+        /// </returns>
         public HitPoint FindClosestHitPoint(Ray ray) {
             HitPoint closestHitPoint = null;
             foreach(ISceneObject sceneObject in SceneObjects) {
@@ -64,6 +123,14 @@ namespace Raytracing {
             return closestHitPoint;
         }
 
+        /// <summary>
+        /// Calculates the colour of a point that is "seen" by a given ray, including ambient light, shadows, diffuse reflection,
+        /// phong reflection and regular reflection with fresnel (depending on the <paramref name="recursionDepth"/>).
+        /// </summary>
+        /// <param name="ray">The ray used to "see"</param>
+        /// <param name="recursionDepth">How far the recursive calculation of reflection should go. 0 means no reflection,
+        /// 1 just regular reflections, 2 reflections of reflections, etc.</param>
+        /// <returns>A vector containing the linear colour values in (R, G, B) order.</returns>
         public Vector3 CalculateColour(Ray ray, int recursionDepth = 1) {
             HitPoint hitPoint = FindClosestHitPoint(ray);
             Vector3 colour = AmbientLight;
@@ -97,6 +164,13 @@ namespace Raytracing {
             return CalculateColour(reflectionRay, recursionDepth);
         }
 
+        /// <summary>
+        /// Checks whether a given <see cref="HitPoint"/> is illuminated by a specific <see cref="LightSource"/>.
+        /// </summary>
+        /// <param name="ray">The ray that created the <see cref="HitPoint"/> (used to adjust the position to reduce graphical noise)</param>
+        /// <param name="hitPoint">The HitPoint</param>
+        /// <param name="lightSource">The LightSource</param>
+        /// <returns>True if the hit point is occluded, false if it is illuminated.</returns>
         private bool IsOccluded(Ray ray, HitPoint hitPoint, LightSource lightSource) {
             Vector3 adjustedPosition = hitPoint.Position - ray.Direction * HIT_POINT_ADJUSTMENT;
             Vector3 L = lightSource.Position - adjustedPosition;
@@ -105,6 +179,10 @@ namespace Raytracing {
             return feelerHitPoint != null && feelerHitPoint.Lambda <= L.Length();
         }
 
+        /// <summary>
+        /// Premade Cornell Box scene.
+        /// </summary>
+        /// <returns>Premade Cornell Box scene.</returns>
         public static Scene CornellBox() {
             Scene cornellBox = new Scene();
             Vector3 specularWall = new Vector3(0, 0, 0);
