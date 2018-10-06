0b3bb9345661f6a4918f3bee48de81abef8e9e3a
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-10-06 13:40:12 +0200

    Documented LightSource

--- Raytracing/LightSource.cs
+++ Raytracing/LightSource.cs
@@ -1,10 +1,27 @@
 ﻿using System.Numerics;
 
 namespace Raytracing {
+
+    /// <summary>
+    /// Represents a point light source. Illuminates a <see cref="Scene"/>.
+    /// </summary>
     public class LightSource {
+
+        /// <summary>
+        /// The light source's position
+        /// </summary>
         public Vector3 Position { get; }
+
+        /// <summary>
+        /// The light source's colour/intensity
+        /// </summary>
         public Vector3 Colour { get; }
 
+        /// <summary>
+        /// Creates a new light source
+        /// </summary>
+        /// <param name="position">The light source's position</param>
+        /// <param name="colour">The light source's colour/intensity</param>
         public LightSource(Vector3 position, Vector3 colour) {
             this.Position = position;
             this.Colour = colour;
