f327a6fdef0c9bd01ae6d78516863fec46a3a36d
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-10-17 15:50:46 +0200

    Fixed pixel width calculation for anti-aliasing

--- Raytracing/Raytracer.cs
+++ Raytracing/Raytracer.cs
@@ -68,7 +68,7 @@ namespace Raytracing {
                     Vector2 pixel = new Vector2((x / (float)(width - 1)) * 2 - 1, (y / (float)(height - 1)) * 2 - 1);
                     Vector3 colour = Colour.Black;
                     if(AASamples > 1) {
-                        Ray[] eyeRays = Camera.CreateEyeRays(pixel, random, AASamples, GaussSigma / width);
+                        Ray[] eyeRays = Camera.CreateEyeRays(pixel, random, AASamples, 2 * GaussSigma / width);
                         foreach(Ray eyeRay in eyeRays) {
                             colour += Scene.CalculateColour(eyeRay, ShadowSamples, ReflectionLevel);
                         }
