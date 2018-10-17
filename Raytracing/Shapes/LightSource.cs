98edf144b3f09129caf13b1450d5077712809e43
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-10-17 10:13:32 +0200

    Implemented LightSource.GenerateShadowFeelers

--- Raytracing/Shapes/LightSource.cs
+++ Raytracing/Shapes/LightSource.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
 
 namespace Raytracing.Shapes {
@@ -35,14 +36,13 @@ namespace Raytracing.Shapes {
             this.Radius = radius;
         }
 
-        public Ray GenerateShadowFeeler(Vector3 origin) {
+        private Ray GenerateShadowFeeler(Vector3 origin, Random random) {
             Vector3 L = Vector3.Normalize(Position - origin);
             Vector3 Nx = Vector3.Normalize(Vector3.Cross(L, new Vector3(0, 1, 0)));
             if(Nx == Vector3.Zero) Nx = Vector3.Normalize(Vector3.Cross(L, new Vector3(0, 0, 1)));
             Vector3 Ny = Vector3.Normalize(Vector3.Cross(L, Nx));
             // Generate a random point
             // TODO Random as parameter?
-            Random random = new Random();
             float r = (float)random.NextDouble();
             float theta = (float)(2 * Math.PI * random.NextDouble());
             float x = (float)(Math.Sqrt(r) * Math.Sin(theta));
@@ -51,6 +51,16 @@ namespace Raytracing.Shapes {
             Vector3 Lprime = p - origin;
             Ray shadowFeeler = new Ray(Position, Lprime);
             return shadowFeeler;
+        }
+
+        public Ray[] GenerateShadowFeelers(Vector3 origin, int n = 1) {
+            n = Math.Max(n, 1);
+            Ray[] rays = new Ray[n];
+            Random random = new Random();
+            for(int i = 0; i < n; i++) {
+                rays[i] = GenerateShadowFeeler(origin, random);
+            }
+            return rays;
         }
     }
 }
