310f42593b5067a8726041b4617a266e9d238bb5
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-10-20 20:53:58 +0200

    Fixed some pathtracer issues

--- Raytracing/Pathtracing/PathtracingScene.cs
+++ Raytracing/Pathtracing/PathtracingScene.cs
@@ -18,20 +18,20 @@ namespace Raytracing.Pathtracing {
         }
 
         public Vector3 CalculateColour(Ray ray, Random random, int recursionDepth) {
-            Vector3 colour = Vector3.Zero;
-            if(recursionDepth > 0) {
-                HitPoint hitPoint = SceneObjects.FindClosestHitPoint(ray);
-                if(hitPoint != null) {
+            HitPoint hitPoint = SceneObjects.FindClosestHitPoint(ray);
+            Vector3 colour = Colour.Black;
+            if(hitPoint != null) {
+                colour = hitPoint.Material.Emissive; ;
+                if(recursionDepth > 0) {
                     Vector3 n = hitPoint.Normal;
                     Vector3 randomVector;
                     do {
                         randomVector = random.NextVector3() * 2 - Vector3.One;
                     } while(randomVector.Length() > 1 && randomVector.Angle(n) >= Math.PI / 2);
                     Vector3 adjustedPosition = hitPoint.Position - ray.Direction * HIT_POINT_ADJUSTMENT;
-                    Ray newRay = new Ray(hitPoint.Position, randomVector);
+                    Ray newRay = new Ray(adjustedPosition, randomVector);
                     float lambert = Vector3.Dot(randomVector, n);
                     Vector3 BRDF = hitPoint.Material.Diffuse;
-                    colour = hitPoint.Material.Emissive;
                     colour += CalculateColour(newRay, random, recursionDepth - 1) * lambert * BRDF;
                     colour /= 1 / (2 * (float)Math.PI);
                 }
