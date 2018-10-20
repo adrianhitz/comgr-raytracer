c68670237ad710fc76a5fe4bc9cc940880edde69
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-10-20 14:41:03 +0200

    Implemented pathtracing

---
+++ Raytracing/Pathtracing/Pathtracer.cs
@@ -0,0 +1,93 @@
+﻿using Raytracing.Helpers;
+using Raytracing.Shapes;
+using System;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace Raytracing.Pathtracing {
+    public class Pathtracer {
+        /// <summary>
+        /// The camera used to take a picture of the scene
+        /// </summary>
+        private Camera Camera { get; }
+
+        /// <summary>
+        /// The scene that is drawn by the raytracer
+        /// </summary>
+        private PathtracingScene Scene { get; }
+
+        /// <summary>
+        /// How far the recursive calculation of reflection should go. 0 means no reflection,
+        /// 1 just regular reflections, 2 reflections of reflections, etc.
+        /// </summary>
+        public int RecursionDepth { get; set; }
+
+
+        /// <summary>
+        /// The numbers of samples for anti-aliasing
+        /// </summary>
+        public int Samples { get; set; }
+
+
+        /// <summary>
+        /// Creates a new raytracer with a camera and a scene
+        /// </summary>
+        /// <param name="camera">A camera</param>
+        /// <param name="scene">A scene</param>
+        public Pathtracer(Camera camera, PathtracingScene scene, int samples = 1, int recursionDepth = 4) {
+            this.Camera = camera;
+            this.Scene = scene;
+            this.Samples = samples;
+            this.RecursionDepth = recursionDepth;
+        }
+
+        /// <summary>
+        /// Calculates all pixels for an image of a given resolution. Returns a two-dimensional array of vectors with (R, G, B) values.
+        /// </summary>
+        /// <param name="width">Image width</param>
+        /// <param name="height">Image height</param>
+        /// <returns>A two-dimensional array of vectors with (R, G, B) values.</returns>
+        public Vector3[,] CalculatePixels(int width, int height) {
+            Vector3[,] pixels = new Vector3[width, height];
+            Parallel.For(0, width, x => {
+                Random random = new Random();
+                for(int y = 0; y < height; y++) {
+                    Vector2 pixel = new Vector2((x / (float)(width - 1)) * 2 - 1, (y / (float)(height - 1)) * 2 - 1);
+                    Vector3 colour = Colour.Black;
+
+                    for(int i = 0; i < Samples; i++) {
+                        Ray eyeRay = Camera.CreateEyeRay(pixel);
+                        colour = Scene.CalculateColour(eyeRay, random, RecursionDepth);
+                    }
+                    
+                    pixels[x, y] = colour;
+                }
+            });
+            return pixels;
+        }
+
+        /// <summary>
+        /// Calculates all pixels for an image of a given resolution. Converts them to SRGB colour space and returns a byte array with all the values.
+        /// </summary>
+        /// <param name="width">Image width</param>
+        /// <param name="height">Image height</param>
+        /// <param name="gamma">Gamma value for transformation to SRGB colour space</param>
+        /// <returns></returns>
+        public byte[] CalculatePixelsByteArray(int width, int height, float gamma = 2.2f) {
+            // TODO This method should probably just return the WriteableBitmap instead
+            Vector3[,] pixels = CalculatePixels(width, height);
+            int stride = pixels.GetLength(0) * 3;
+            byte[] bytes = new byte[stride * height];
+            for(int y = 0; y < pixels.GetLength(0); y++) {
+                for(int x = 0; x < pixels.GetLength(1); x++) {
+                    int i = (y * pixels.GetLength(0) + x) * 3;
+                    Vector3 colourSRGB = pixels[x, y].ToSRGB(gamma);
+                    bytes[i] = (byte)(colourSRGB.Z);
+                    bytes[i + 1] = (byte)(colourSRGB.Y);
+                    bytes[i + 2] = (byte)(colourSRGB.X);
+                }
+            }
+            return bytes;
+        }
+    }
+}
---
+++ Raytracing/Pathtracing/PathtracingScene.cs
@@ -0,0 +1,42 @@
+﻿using Raytracing.Acceleration;
+using Raytracing.Helpers;
+using Raytracing.Shapes;
+using System;
+using System.Numerics;
+
+namespace Raytracing.Pathtracing {
+    public class PathtracingScene {
+        private ISceneObjectContainer SceneObjects { get; }
+        private static readonly float HIT_POINT_ADJUSTMENT = 0.01f;
+
+        public PathtracingScene(ISceneObjectContainer sceneObjects) {
+            this.SceneObjects = sceneObjects;
+        }
+
+        public HitPoint FindClosestHitPoint(Ray ray) {
+            return SceneObjects.FindClosestHitPoint(ray);
+        }
+
+        public Vector3 CalculateColour(Ray ray, Random random, int recursionDepth) {
+            Vector3 colour = Vector3.Zero;
+            if(recursionDepth > 0) {
+                HitPoint hitPoint = SceneObjects.FindClosestHitPoint(ray);
+                if(hitPoint != null) {
+                    Vector3 n = hitPoint.Normal;
+                    Vector3 randomVector;
+                    do {
+                        randomVector = random.NextVector3() * 2 - Vector3.One;
+                    } while(randomVector.Length() > 1 && randomVector.Angle(n) >= Math.PI / 2);
+                    Vector3 adjustedPosition = hitPoint.Position - ray.Direction * HIT_POINT_ADJUSTMENT;
+                    Ray newRay = new Ray(hitPoint.Position, randomVector);
+                    float lambert = Vector3.Dot(randomVector, n);
+                    Vector3 BRDF = hitPoint.Material.Diffuse;
+                    colour = hitPoint.Material.Emissive;
+                    colour += CalculateColour(newRay, random, recursionDepth - 1) * lambert * BRDF;
+                    colour /= 1 / (2 * (float)Math.PI);
+                }
+            }
+            return colour;
+        }
+    }
+}
