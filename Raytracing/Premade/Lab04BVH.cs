26dfa1675cb08fbe3aca194817cfc02b4a7b1a99
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-10-22 15:22:27 +0200

    Updated Lab04 BVH premade scene

--- Raytracing/Premade/Lab04BVH.cs
+++ Raytracing/Premade/Lab04BVH.cs
@@ -16,40 +16,41 @@ namespace Raytracing.Premade {
             }
 
             public static Scene Scene(Scene.AccelerationStructure accelerationStructure = Raytracing.Scene.AccelerationStructure.BVH) {
-                int n = 10;
+                int n = 20;
                 List<ISceneObject> sceneObjects = new List<ISceneObject>();
                 Random random = new Random();
 
                 for(int i = 0; i < n; i++) {
                     for(int j = 0; j < n; j++) {
                         Vector3 diffuse = random.NextVector3();
-                        Vector3 specular = new Vector3(0.8f, 0.8f, 0.8f);
+                        Vector3 specular = new Vector3(0.1f, 0.1f, 0.1f);
                         Material material = new Material(diffuse, specular);
-                        Vector3 position = new Vector3((3f / n) * i - 1.5f, (3f / n) * j - 1.5f, 0);
-                        float r = (3f / n) * (0.9f / 2);
+                        Vector3 position = new Vector3((6f / n) * i - 2f, (6f / n) * j - 2f, 0);
+                        float r = (6f / n) * (0.95f / 2);
 
                         sceneObjects.Add(new Sphere(position, r, material));
                     }
                 }
+                /*
                 for(int i = 0; i < n; i++) {
                     for(int j = 0; j < n; j++) {
                         Vector3 diffuse = random.NextVector3();
-                        Vector3 specular = new Vector3(0.8f, 0.8f, 0.8f);
-                        Vector3 reflective = new Vector3(0.5f, 0.5f, 0.5f);
-                        Material material = new Material(diffuse, specular, reflective);
+                        Vector3 specular = new Vector3(0.3f, 0.3f, 0.3f);
+                        Material material = new Material(diffuse, specular);
                         Vector3 position = new Vector3((3f / n) * (i + 0.5f) - 1.5f, (3f / n) * (j + 0.5f) - 1.5f, 0.2f);
                         float r = (3f / n) * (0.95f / 2);
 
                         sceneObjects.Add(new Sphere(position, r, material));
                     }
                 }
+                */
                 sceneObjects.Add(new Sphere(new Vector3(0, 0, 1003f), 1000, new Material(Colour.White, new Vector3(0.1f, 0.1f, 0.1f))));
 
                 LightSource[] lightSources = new LightSource[] {
                     new LightSource(new Vector3(-1, -1, -2), Colour.White)
                 };
 
-                return new Scene(sceneObjects, lightSources);
+                return new Scene(sceneObjects, lightSources, accelerationStructure);
             }
         }
     }
