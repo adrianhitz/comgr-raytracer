f3d5e4f8d87f0dba2cdac1387b1542ec966f6e70
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-10-22 07:03:51 +0200

    Updated Lab04 premade scene

--- Raytracing/Premade/Lab04Textures.cs
+++ Raytracing/Premade/Lab04Textures.cs
@@ -15,8 +15,8 @@ namespace Raytracing.Premade {
             }
 
             public static Scene Scene() {
-                Vector3 specularWall = new Vector3(0.05f, 0.05f, 0.05f);
-                Vector3 specularSphere = new Vector3(0.15f, 0.15f, 0.15f);
+                Vector3 specularWall = new Vector3(0.1f, 0.1f, 0.1f);
+                Vector3 specularSphere = new Vector3(0.01f, 0.01f, 0.01f);
                 Material whiteWall = new Material(Colour.White, specularWall);
                 var cornellBoxSpheres = new List<ISceneObject> {
                     new Sphere(new Vector3(1001, 0, 0), 1000, new Material(Colour.Red, specularWall)),
@@ -24,12 +24,12 @@ namespace Raytracing.Premade {
                     new Sphere(new Vector3(0, 0, 1001), 1000, whiteWall),
                     new Sphere(new Vector3(0, -1001, 0), 1000, whiteWall),
                     new Sphere(new Vector3(0, 1001, 0), 1000, whiteWall),
-                    new Sphere(new Vector3(0, 0, -1005), 1000,whiteWall),
+                    new Sphere(new Vector3(0, 0, -1005), 1000, whiteWall),
                     new Sphere(new Vector3(0.6f, 0.7f, -0.6f), 0.3f, new Material(Colour.Yellow, specularSphere, new Texture(@"Resources\pluto.jpg", rotationOffset: 0.8f*(float)Math.PI))),
-                new Sphere(new Vector3(-0.3f, 0.4f, 0.3f), 0.6f, new Material(Colour.LightCyan, specularSphere, new Texture(@"Resources\earth.jpg", rotationOffset: 1.2f*(float)Math.PI)))
+                    new Sphere(new Vector3(-0.3f, 0.4f, 0.3f), 0.6f, new Material(Colour.LightCyan, specularSphere, new Texture(@"Resources\earth.jpg", rotationOffset: 1.2f*(float)Math.PI)))
                 };
                 var cornellBoxLight = new List<LightSource> {
-                    new LightSource(new Vector3(0.5f, -0.6f, 0), Colour.White)
+                    new LightSource(new Vector3(0.5f, -0.6f, -1.3f), Colour.White)
                 };
                 return new Scene(cornellBoxSpheres, cornellBoxLight);
             }
