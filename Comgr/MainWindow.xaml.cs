ca6068852c5312e1c1ee3bff9edcd95e6f36d98a
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-10-23 11:41:38 +0200

    Added premade scene for Lab05

--- Comgr/MainWindow.xaml.cs
+++ Comgr/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace Comgr {
     /// </summary>
     public partial class MainWindow : Window {
 
-        private const int imageResolution = 400;
+        private const int imageResolution = 800;
 
         public MainWindow() {
             InitializeComponent();
@@ -29,8 +29,11 @@ namespace Comgr {
             Task task = new Task(() => {
                 Stopwatch stopwatch = new Stopwatch();
                 stopwatch.Start();
-                byte[] pixels = Lab04BVH();
+                // byte[] pixels = Lab03();
+                // byte[] pixels = Lab04BVH();
                 // byte[] pixels = Lab04Textures();
+                // byte[] pixels = Lab05();
+                byte[] pixels = PathtracingImage();
                 this.Dispatcher.Invoke(() => {
                     writeableBitmap.WritePixels(new Int32Rect(0, 0, imageResolution, imageResolution), pixels, imageResolution * 3, 0);
                     stopwatch.Stop();
@@ -52,7 +55,7 @@ namespace Comgr {
 
         private static byte[] PathtracingImage() {
             Pathtracer pathtracer = new Pathtracer(Premade.CornellBox.Camera(), Premade.PathCornellBox.Scene()) {
-                Samples = 128,
+                Samples = 512,
                 RecursionDepth = 4
             };
             return pathtracer.CalculatePixelsByteArray(imageResolution, imageResolution);
@@ -70,6 +73,11 @@ namespace Comgr {
 
         private static byte[] Lab04Textures() {
             Raytracer raytracer = new Raytracer(Premade.Lab04Textures.Camera(), Premade.Lab04Textures.Scene());
+            return raytracer.CalculatePixelsByteArray(imageResolution, imageResolution);
+        }
+
+        private static byte[] Lab05() {
+            Raytracer raytracer = new Raytracer(Premade.Lab05.Camera(), Premade.Lab05.Scene(), 30, 30);
             return raytracer.CalculatePixelsByteArray(imageResolution, imageResolution);
         }
     }
---
+++ Raytracing/Premade/Lab05.cs
@@ -0,0 +1,41 @@
+﻿using Raytracing.Helpers;
+using Raytracing.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raytracing.Premade {
+    public static partial class Premade {
+        public static class Lab05 {
+            public static Camera Camera() {
+                Vector3 position = new Vector3(0, 0, -4);
+                Vector3 lookAt = new Vector3(0, 0, 6);
+                float fov = (float)Math.PI / 5;
+                return new Camera(position, lookAt, fov);
+            }
+
+            public static Scene Scene() {
+                Vector3 specularWall = new Vector3(0.01f, 0.01f, 0.01f);
+                Vector3 specularSphere = new Vector3(0.1f, 0.1f, 0.1f);
+                Material whiteWall = new Material(Colour.White, specularWall);
+                var cornellBoxSpheres = new List<ISceneObject> {
+                    new Sphere(new Vector3(1001, 0, 0), 1000, new Material(Colour.Red, specularWall)),
+                    new Sphere(new Vector3(-1001, 0, 0), 1000, new Material(Colour.Blue, specularWall)),
+                    new Sphere(new Vector3(0, 0, 1001), 1000, whiteWall),
+                    new Sphere(new Vector3(0, -1001, 0), 1000, whiteWall),
+                    new Sphere(new Vector3(0, 1001, 0), 1000, whiteWall),
+                    new Sphere(new Vector3(0, 0, -1005), 1000, whiteWall),
+                    new Sphere(new Vector3(0.6f, 0.7f, -0.6f), 0.3f, new Material(Colour.Yellow, specularSphere)),
+                    new Sphere(new Vector3(-0.3f, 0.4f, 0.3f), 0.6f, new Material(Colour.LightCyan, specularSphere))
+                };
+                var cornellBoxLight = new List<LightSource> {
+                    new LightSource(new Vector3(0.5f, -0.6f, 0), Colour.White, 0.35f)
+                };
+                return new Scene(cornellBoxSpheres, cornellBoxLight);
+            }
+        }
+    }
+}
