7404f8d8621f611d7d763a1ca1756ea23bddc937
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-10-24 08:10:48 +0200

    Updated Lab06 premade scene

--- Raytracing/Premade/Lab06.cs
+++ Raytracing/Premade/Lab06.cs
@@ -18,15 +18,15 @@ namespace Raytracing.Premade {
             public static PathtracingScene Scene(Scene.AccelerationStructure accelerationStructure = Raytracing.Scene.AccelerationStructure.None) {
                 Material whiteMaterial = new Material(Colour.White * 0.9f, Colour.Black, Colour.Black);
                 var cornellBoxSpheres = new List<ISceneObject> {
-                    new Sphere(new Vector3(1001, 0, 0), 1000, new Material(Colour.Red, Colour.Black)),
-                    new Sphere(new Vector3(-1001, 0, 0), 1000, new Material(Colour.Blue, Colour.Black)),
+                    new Sphere(new Vector3(1001, 0, 0), 1000, new Material(Colour.Red * 0.9f, Colour.Black)),
+                    new Sphere(new Vector3(-1001, 0, 0), 1000, new Material(Colour.Blue * 0.9f, Colour.Black)),
                     new Sphere(new Vector3(0, 0, 1001), 1000, whiteMaterial),
                     new Sphere(new Vector3(0, -1001, 0), 1000, whiteMaterial),
                     new Sphere(new Vector3(0, 1001, 0), 1000, whiteMaterial),
                     new Sphere(new Vector3(0, 0, -1005), 1000, whiteMaterial),
                     new Sphere(new Vector3(0.6f, 0.7f, -0.6f), 0.3f, new Material(Colour.Yellow * 0.9f, Colour.Black)),
                     new Sphere(new Vector3(-0.3f, 0.4f, 0.3f), 0.6f, new Material(Colour.LightCyan * 0.9f, Colour.Black, Colour.Black)),
-                    new Sphere(new Vector3(0, -10.99f, 0), 10, new Material(Colour.Black, Colour.Black, Colour.White))
+                    new Sphere(new Vector3(0, -10.99f, 0), 10, new Material(Colour.Black, Colour.Black, Colour.White * 0.6f))
                 };
                 SceneObjectList sceneObjects = new SceneObjectList(cornellBoxSpheres);
                 return new PathtracingScene(sceneObjects);
